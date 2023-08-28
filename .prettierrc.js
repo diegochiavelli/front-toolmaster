b7a658edc45d253bcdf06889bd6114daf57d00f2
diegochiavelli <44420022+diegochiavelli@users.noreply.github.com>
2023-08-27 21:30:09 -0300

    feat: adionando eslint e prettier

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, // adiciona o ; 
+    trailingComma: 'all', // adicona a , 
+    singleQuote: true, // adiconar aspas simples ''
+    printWidth: 100, // tamanho de caracteres por linha 
+    tabWidth: 2, // tamanho da tab por espaços 
+};
